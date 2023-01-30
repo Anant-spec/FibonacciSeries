15edb1e7445f92ce6fa1c8b8eb5030945715d946
Anant Hinge <ananthinge135@gmail.com>
2023-01-30 14:09:38 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FibonacciSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Fibonacci series up to " + n + ":");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(Fibonacci(i) + "");
+            }
+            Console.ReadLine();
+        }
+
+        static int Fibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+    }
+}
